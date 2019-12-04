c2c52ea835edf158ac2e9a2e5a5e6f810ef3cbe2
velagin <elagin-vasilii@yandex.ru>
2019-12-04 18:48:42 +0300

    'task1'

---
+++ lesson1/task1.py
@@ -0,0 +1,12 @@
+a = 12345
+b = 'Hello world!'
+
+print(a)
+print(b)
+
+name = input('Введите Ваше имя: ')
+patronimic = input('Введите Ваше отчество: ')
+age = int(input('Введите Ваш возраст: '))
+experience = int(input('Введите Ваш стаж работы, лет: '))
+
+print(f'Добрый день {name} {patronimic}! Ваш возраст {age}, а стаж {experience} лет')
